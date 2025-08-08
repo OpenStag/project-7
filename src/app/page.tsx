e8f8974f84bffe93e6f0c1eb58adc1780aa7d00a
janitha dissanayaka <janithadissnaykaa@gmail.com>
2025-08-08 21:09:16 +0530

    adding contactus component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import ContactUs from "../components/contactus"
 
 export default function Home() {
   return (
-    <div>
-      
+    <div >
+      <ContactUs/>
     </div>
   );
 }
