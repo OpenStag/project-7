ef698946e3011a9142fbcd9600c6545b8f9cf89c
Nisansala23 <tmnsthilakarathne@students.nsbm.ac.lk>
2025-08-09 15:11:42 +0200

    Feat:create Home page with Hero section and about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Home from '../components/Home'; // Correctly import your component
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
-      
+      <Home />
     </div>
   );
 }
